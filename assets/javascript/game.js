86f346b9c4a65bf8ed055f235e397c4f832e0723
Noel Nevarez <noel3740@gmail.com>
2018-10-13 13:59:23 -0500

    Converting javascript code to model. Adding background images.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,102 +1,121 @@
-//Declare global variables
-var numGuessRemain = 12;
-var words = [];
-var lettersGuessed = [];
-var lettersGuessedCorrectly = [];
-var wins = 0;
-var currentWord = "";
-var currentDisplayWord = "";
+//Declare object
+var wordGuessGame = {
+    numGuessRemain: 12,
+    words: [],
+    letterGuessed: [],
+    lettersGuessedCorrectly: [],
+    wins: 0,
+    currentWord: "",
+    currentDisplayWord: "",
 
-//Initialize the words array
-function initializeWords(){
-    words = ["unaccountable", "mom", "type", "rustic", "violet", "receive", "gray", "draw", "read", "fallacious", "advertisement", "government", "umbrella", "sable", "bait", "invite", "bell", "belligerent", "building", "willing"];
-}
+    //Initialize the words array
+    initializeWords: function () {
+        this.words = ["ukulele", "aloha", "poke", "mahalo", "hawaii", "maui", "surf", "turtles", "whales", "Oahu", "Honolulu", "Waikiki", "volcano", "beaches", "harbor", "waterfalls", "mountains", "dolphins", "hula", "leis"];
+    },
 
-//Get next word for the user to guess
-function getWord() {
-    //Reset values
-    currentWord = "";
-    numGuessRemain = 12;
-    lettersGuessed = [];
-    lettersGuessedCorrectly = [];
+    //Get next word for the user to guess
+    getWord: function() {
+        //Reset values
+        this.currentWord = "";
+        this.numGuessRemain = 12;
+        this.lettersGuessed = [];
+        this.lettersGuessedCorrectly = [];
+    
+        //Words array has values
+        if (this.words.length > 0) {
+            //Get random array index number
+            var randIndex = Math.floor(Math.random() * this.words.length);
+            //Assign the word to the current word variable
+            this.currentWord = this.words[randIndex].toLowerCase();
+            //Remove the word from the array list so it doesn't get picked again
+            this.words.splice(randIndex, 1);
+    
+            //Debugging. Need to remove
+            console.log("Current Word: " + this.currentWord);
+            console.log("Words left: " + this.words.length);
+        }
+        //words array does not have values 
+        else { 
+            //Re-initilze the words array
+            this.initializeWords();
+            //Re-run the get words function to get the next word now that the words array is refreshed
+            this.getWord();
+        }
+    },
 
-    //Words array has values
-    if (words.length > 0) {
-        //Get random array index number
-        var randIndex = Math.floor(Math.random() * words.length);
-        //Assign the word to the current word variable
-        currentWord = words[randIndex];
-        //Remove the word from the array list so it doesn't get picked again
-        words.splice(randIndex, 1);
+    //Function to run when the player wins
+    onWin: function () {
+        //Play win audio
+        document.getElementById("winAudio").play();
+        //Increment the wins by 1
+        this.wins++;
+        //Get a new word
+        this.getWord();
+        //Refresh the screen fields
+        this.refreshScreenFields();
+    },
 
-        //Debugging. Need to remove
-        console.log("Current Word: " + currentWord);
-        console.log("Words left: " + words.length);
-    }
-    //words array does not have values 
-    else { 
-        //Re-initilze the words array
-        initializeWords();
-        //Re-run the get words function to get the next word now that the words array is refreshed
-        getWord();
-    }
-}
+    //Function that refreshes all the dynamic html fields
+    refreshScreenFields: function () {
+        //Set the current display word
+        this.setCurrentDisplayWord();
 
-//Get an array of index locations in the current word that the user guessed correctly
-function getIndexOfCorrectLetters (letterGuessed) {
-    var correctLetterIndexes = [];
+        //Set the html element contents to their coresponding variable values
+        if (this.lettersGuessed.length >0){
+            document.getElementById("lettersGuessedText").textContent = this.lettersGuessed.toString();
+        }
+        else {
+            document.getElementById("lettersGuessedText").textContent = "";
+        }
 
-    //Loop through each letter in the current word
-    for (var i=0; i < currentWord.length; i++){
-        //If the current character in the current word is equal to the letter guessed then add it to the array
-        if (currentWord[i].toLowerCase() == letterGuessed.toLowerCase()) {
-            correctLetterIndexes.push(i);
+        document.getElementById("winsText").textContent = this.wins;
+        document.getElementById("numGuessRemainText").textContent = this.numGuessRemain;
+        document.getElementById("currentWordText").textContent = this.currentDisplayWord;
+    },
+
+    //Get the word to display on the screen (with "_")
+    setCurrentDisplayWord: function () {
+        //Set the current display word to the current word variable
+        this.currentDisplayWord = this.currentWord;
+
+        //Create a regular expression search pattern to search for letters that were not guessed correctly
+        var searchLetters = this.lettersGuessedCorrectly.toString().replace(",", "");
+        var searchPattern = new RegExp("[^" + searchLetters + "]", "g");
+    
+        //Using the regular expression above, replace any letters that have not been guessed yet with "_"
+        this.currentDisplayWord = this.currentDisplayWord.replace(searchPattern, "_");
+    },
+
+    //Process the user's guess
+    processUserGuess: function(usersGuess) {
+        //Add the letter guessed by the user to the letterGuessed array
+        this.lettersGuessed.push(usersGuess);
+
+        //Current word contains the letter guessed
+        if (this.currentWord.indexOf(usersGuess) >=0) {
+            //Add letter to array of letters guessed correctly
+            this.lettersGuessedCorrectly.push(usersGuess);
+
+            //Set the display word text (with "_")
+            this.setCurrentDisplayWord();
+
+            //If the current display word text does not contain "_" then the user wins
+            if (this.currentDisplayWord.indexOf("_") < 0){
+                this.onWin();
+            }
+        }
+        //User guessed incorrectly
+        else {
+            this.numGuessRemain--;
+
+            if (this.numGuessRemain == 0) {
+                //Get a new word
+                this.getWord();
+            }
         }
     }
+};
 
-    return correctLetterIndexes;
-}
-
-//Function to run when the player wins
-function onWin() {
-    //Play win audio
-    document.getElementById("winAudio").play();
-    //Increment the wins by 1
-    wins++;
-    //Get a new word
-    getWord();
-    //Refresh the screen fields
-    refreshScreenFields();
-}
-
-//Function that refreshes all the dynamic html fields
-function refreshScreenFields(){
-    //Set the current display word
-    setCurrentDisplayWord();
-
-    //Set the html element contents to their coresponding variable values
-    if (lettersGuessed.length >0){
-        document.getElementById("lettersGuessedText").textContent = lettersGuessed.toString();
-    }
-    else {
-        document.getElementById("lettersGuessedText").textContent = "";
-    }
-
-    document.getElementById("winsText").textContent = wins;
-    document.getElementById("numGuessRemainText").textContent = numGuessRemain;
-    document.getElementById("currentWordText").textContent = currentDisplayWord;
-}
-
-//Get the word to display on the screen (with "_")
-function setCurrentDisplayWord () {
-
-    currentDisplayWord = currentWord;
-
-    var searchLetters = lettersGuessedCorrectly.toString().replace(",", "");
-    var searchPattern = new RegExp("[^" + searchLetters + "]", "g");
-
-    currentDisplayWord = currentDisplayWord.replace(searchPattern, "_");
-}
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
@@ -105,47 +124,23 @@ document.onkeyup = function(event) {
     var usersGuessKeyCode = event.keyCode;
 
     if (//Users key press was not already guessed
-        lettersGuessed.indexOf(usersGuess.toLowerCase()) < 0 &&
+        wordGuessGame.lettersGuessed.indexOf(usersGuess.toLowerCase()) < 0 &&
         //Key pressed is a letter
         (usersGuessKeyCode >= 65 && usersGuessKeyCode <= 90)) {
 
-            //Add the letter guessed by the user to the letterGuessed array
-            lettersGuessed.push(usersGuess);
-
-            //Current word contains the letter guessed
-            if (currentWord.indexOf(usersGuess) >=0) {
-                //Add letter to array of letters guessed correctly
-                lettersGuessedCorrectly.push(usersGuess);
-
-                //Set the display word text (with "_")
-                setCurrentDisplayWord();
-
-                //If the current display word text does not contain "_" then the user wins
-                if (currentDisplayWord.indexOf("_") < 0){
-                    onWin();
-                }
-            }
-            //User guessed incorrectly
-            else {
-                numGuessRemain--;
-
-                if (numGuessRemain == 0) {
-                    //Get a new word
-                    getWord();
-                }
-            }
-
+            //Process the user's guess
+            wordGuessGame.processUserGuess(usersGuess);
 
             //Refresh the screen fields
-            refreshScreenFields();
+            wordGuessGame.refreshScreenFields();
         }
 }
 
 //Initialize the words array
-initializeWords();
+wordGuessGame.initializeWords();
 //Get next word to guess
-getWord();
+wordGuessGame.getWord();
 //Refresh the screen fields
-refreshScreenFields();
+wordGuessGame.refreshScreenFields();
 
 
